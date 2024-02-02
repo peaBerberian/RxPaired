8a205e40b6e3b3c14c931143a8fb4d670c17f578
Florent <florent.bouisset@gmail.com>
2024-02-02 12:56:43 +0100

    time representation also affects other modules

--- inspector/src/modules/manifest_parsing_time_history.ts
+++ inspector/src/modules/manifest_parsing_time_history.ts
@@ -1,97 +1,116 @@
 import strHtml from "str-html";
-import { InspectorState, LogViewState, STATE_PROPS } from "../constants";
+import { ConfigState, InspectorState, LogViewState, STATE_PROPS } from "../constants";
 import ObservableState, { UPDATE_TYPE } from "../observable_state";
+import { convertDateToLocalISOString } from "../utils";
 
 const MAX_ELEMENTS = 50;
 
 export default function ManifestParsingTimeHistoryModule({
   state,
   logView,
+  configState,
 }: {
   state: ObservableState<InspectorState>;
   logView: ObservableState<LogViewState>;
+  configState: ObservableState<ConfigState>;
 }) {
   const manifestParsingHistoryElt = strHtml`<div>No manifest parsing information</div>`;
   const moduleBodyElt = strHtml`<div class="state-history-body module-body">
     ${manifestParsingHistoryElt}
   </div>`;
+  const renderParsingTimeHistory = () => {
+    manifestParsingHistoryElt.innerHTML = "";
+
+    // TODO more optimized: rely on push
+    const manifestParsingTimeHistory = state.getCurrentState(
+      STATE_PROPS.MANIFEST_PARSING_TIME_HISTORY
+    );
+
+    if (manifestParsingTimeHistory === undefined) {
+      displayNoStateInformation();
+      return;
+    }
+
+    const tableElt = strHtml`<table>
+      <tr>
+        <th>TS</th>
+        <th>Parsing Time (ms)</th>
+        <th>Time since prev. parsing (ms)</th>
+      </tr>
+    </table>`;
+    for (let i = manifestParsingTimeHistory.length - 1; i >= 0; i--) {
+      const info = manifestParsingTimeHistory[i];
+      let innerText: string;
+      if (configState.getCurrentState(STATE_PROPS.TIME_REPRESENTATION) === "date") {
+        const dateAtPageLoad = logView.getCurrentState(STATE_PROPS.DATE_AT_PAGE_LOAD) ?? 0
+        innerText = convertDateToLocalISOString(new Date(info.timestamp + dateAtPageLoad));
+      } else {
+        innerText = `${info.timestamp}`
+      }
+
+      const timestampElt = strHtml`<td>${innerText}</td>`;
+      timestampElt.style.cursor = "pointer";
+      timestampElt.style.textDecoration = "underline";
+      timestampElt.onclick = () => {
+        logView.updateState(
+          STATE_PROPS.LOG_MIN_TIMESTAMP_DISPLAYED,
+          UPDATE_TYPE.REPLACE,
+          0
+        );
+        logView.updateState(  
+          STATE_PROPS.LOG_MAX_TIMESTAMP_DISPLAYED,
+          UPDATE_TYPE.REPLACE,
+          info.timestamp
+        );
+        logView.commitUpdates();
+      };
+      tableElt.appendChild(
+        strHtml`<tr>
+          ${timestampElt}
+          <td>
+            ${info.timeMs.toFixed(2)}
+          </td>
+          <td>
+            ${
+              i === 0
+                ? "-"
+                : (
+                    info.timestamp -
+                    manifestParsingTimeHistory[i - 1].timestamp
+                  ).toFixed(2)
+            }
+          </td>
+        </tr>`
+      );
+      if (tableElt.childNodes.length >= MAX_ELEMENTS - 1) {
+        manifestParsingHistoryElt.appendChild(tableElt);
+        moduleBodyElt.classList.remove("empty");
+        return;
+      }
+    }
+    if (tableElt.childNodes.length === 1) {
+      displayNoStateInformation();
+      return;
+    }
+    manifestParsingHistoryElt.appendChild(tableElt);
+    moduleBodyElt.classList.remove("empty");
+  }
   const unsubscribeHistory = state.subscribe(
     STATE_PROPS.MANIFEST_PARSING_TIME_HISTORY,
-    () => {
-      manifestParsingHistoryElt.innerHTML = "";
+    renderParsingTimeHistory,
+    true
+  );
 
-      // TODO more optimized: rely on push
-      const manifestParsingTimeHistory = state.getCurrentState(
-        STATE_PROPS.MANIFEST_PARSING_TIME_HISTORY
-      );
-
-      if (manifestParsingTimeHistory === undefined) {
-        displayNoStateInformation();
-        return;
-      }
-
-      const tableElt = strHtml`<table>
-        <tr>
-          <th>TS</th>
-          <th>Parsing Time (ms)</th>
-          <th>Time since prev. parsing (ms)</th>
-        </tr>
-      </table>`;
-      for (let i = manifestParsingTimeHistory.length - 1; i >= 0; i--) {
-        const info = manifestParsingTimeHistory[i];
-        const timestampElt = strHtml`<td>${info.timestamp}</td>`;
-        timestampElt.style.cursor = "pointer";
-        timestampElt.style.textDecoration = "underline";
-        timestampElt.onclick = () => {
-          logView.updateState(
-            STATE_PROPS.LOG_MIN_TIMESTAMP_DISPLAYED,
-            UPDATE_TYPE.REPLACE,
-            0
-          );
-          logView.updateState(
-            STATE_PROPS.LOG_MAX_TIMESTAMP_DISPLAYED,
-            UPDATE_TYPE.REPLACE,
-            info.timestamp
-          );
-          logView.commitUpdates();
-        };
-        tableElt.appendChild(
-          strHtml`<tr>
-            ${timestampElt}
-            <td>
-              ${info.timeMs.toFixed(2)}
-            </td>
-            <td>
-              ${
-                i === 0
-                  ? "-"
-                  : (
-                      info.timestamp -
-                      manifestParsingTimeHistory[i - 1].timestamp
-                    ).toFixed(2)
-              }
-            </td>
-          </tr>`
-        );
-        if (tableElt.childNodes.length >= MAX_ELEMENTS - 1) {
-          manifestParsingHistoryElt.appendChild(tableElt);
-          moduleBodyElt.classList.remove("empty");
-          return;
-        }
-      }
-      if (tableElt.childNodes.length === 1) {
-        displayNoStateInformation();
-        return;
-      }
-      manifestParsingHistoryElt.appendChild(tableElt);
-      moduleBodyElt.classList.remove("empty");
-    },
+  const unsubscribeTimeRepresentation = configState.subscribe(
+    STATE_PROPS.TIME_REPRESENTATION,
+    renderParsingTimeHistory,
     true
   );
   return {
     body: moduleBodyElt,
     destroy() {
       unsubscribeHistory();
+      unsubscribeTimeRepresentation();
     },
   };
 
--- inspector/src/modules/request_history.ts
+++ inspector/src/modules/request_history.ts
@@ -1,5 +1,6 @@
 import strHtml from "str-html";
 import {
+  ConfigState,
   InspectorState,
   LogViewState,
   RequestInformation,
@@ -7,6 +8,7 @@ import {
 } from "../constants";
 import ObservableState, { UPDATE_TYPE } from "../observable_state";
 import { ModuleFunction } from ".";
+import { convertDateToLocalISOString } from "../utils";
 
 const MAX_REQ_ELEMENTS = 50;
 
@@ -16,9 +18,11 @@ export default function generateRequestHistoryModule(
   return function RequestInformationModule({
     state,
     logView,
+    configState,
   }: {
     state: ObservableState<InspectorState>;
     logView: ObservableState<LogViewState>;
+    configState: ObservableState<ConfigState>;
   }) {
     const requestDataElt = strHtml`<div>No request information</div>`;
     const moduleBodyElt = strHtml`<div class="request-history-body module-body">
@@ -45,137 +49,151 @@ export default function generateRequestHistoryModule(
       updatePendingTimeElt
     );
 
-    const unsubscribeHistory = state.subscribe(
-      stateProp,
-      () => {
-        requestDataElt.innerHTML = "";
-        pendingRequestInfo = null;
+    const renderRequestHistory = () => {
+      requestDataElt.innerHTML = "";
+      pendingRequestInfo = null;
 
-        // TODO more optimized: rely on push
-        const requestInfo = state.getCurrentState(stateProp);
+      // TODO more optimized: rely on push
+      const requestInfo = state.getCurrentState(stateProp);
 
-        if (requestInfo === undefined) {
-          displayNoRequestInformation();
-          return;
+      if (requestInfo === undefined) {
+        displayNoRequestInformation();
+        return;
+      }
+      let canStillReceivePending = true;
+      const tableElt = strHtml`<table>
+      <tr>
+        <th>Log TS</th>
+        <th>Req. status</th>
+        <th>Req. duration (ms)</th>
+        <th>Period id</th>
+        <th>Representation id</th>
+        <th>Seg. start (s)</th>
+        <th>Seg. duration (s)</th>
+      </tr>
+    </table>`;
+      for (let i = requestInfo?.length - 1; i >= 0; i--) {
+        const req = requestInfo[i];
+
+        let innerText: string;
+        if (configState.getCurrentState(STATE_PROPS.TIME_REPRESENTATION) === "date") {
+          const dateAtPageLoad = logView.getCurrentState(STATE_PROPS.DATE_AT_PAGE_LOAD) ?? 0
+          innerText = convertDateToLocalISOString(new Date(req.timestamp + dateAtPageLoad));
+        } else {
+          innerText = `${req.timestamp}`
         }
-        let canStillReceivePending = true;
-        const tableElt = strHtml`<table>
-        <tr>
-          <th>Log TS</th>
-          <th>Req. status</th>
-          <th>Req. duration (ms)</th>
-          <th>Period id</th>
-          <th>Representation id</th>
-          <th>Seg. start (s)</th>
-          <th>Seg. duration (s)</th>
-        </tr>
-      </table>`;
-        for (let i = requestInfo?.length - 1; i >= 0; i--) {
-          const req = requestInfo[i];
+        const timestampElt = strHtml`<td>${innerText}</td>`;
+        timestampElt.style.cursor = "pointer";
+        timestampElt.style.textDecoration = "underline";
+        timestampElt.onclick = () => {
+          logView.updateState(
+            STATE_PROPS.LOG_MIN_TIMESTAMP_DISPLAYED,
+            UPDATE_TYPE.REPLACE,
+            0
+          );
+          logView.updateState(
+            STATE_PROPS.LOG_MAX_TIMESTAMP_DISPLAYED,
+            UPDATE_TYPE.REPLACE,
+            req.timestamp
+          );
+          logView.commitUpdates();
+        };
 
-          const timestampElt = strHtml`<td>${req.timestamp}</td>`;
-          timestampElt.style.cursor = "pointer";
-          timestampElt.style.textDecoration = "underline";
-          timestampElt.onclick = () => {
-            logView.updateState(
-              STATE_PROPS.LOG_MIN_TIMESTAMP_DISPLAYED,
-              UPDATE_TYPE.REPLACE,
-              0
-            );
-            logView.updateState(
-              STATE_PROPS.LOG_MAX_TIMESTAMP_DISPLAYED,
-              UPDATE_TYPE.REPLACE,
-              req.timestamp
-            );
-            logView.commitUpdates();
+        if (canStillReceivePending && req.eventType === "start") {
+          pendingRequestInfo = {
+            element: strHtml`<td> - </td>`,
+            timestamp: req.timestamp,
           };
-
-          if (canStillReceivePending && req.eventType === "start") {
-            pendingRequestInfo = {
-              element: strHtml`<td> - </td>`,
-              timestamp: req.timestamp,
-            };
-            updatePendingTimeElt();
-            const newElt = strHtml`<tr>
-            ${timestampElt}
-            <td>pending</td>
-            ${pendingRequestInfo.element}
-            <td>${req.periodId}</td>
-            <td>${req.representationId}</td>
-            <td>
-              ${req.segmentStart === -1 ? "init" : req.segmentStart.toFixed(2)}
-            </td>
-            <td>
-              ${
-                req.segmentDuration === -1
-                  ? "-"
-                  : req.segmentDuration.toFixed(2)
-              }
-            </td>
-          </tr>`;
-            tableElt.appendChild(newElt);
-            canStillReceivePending = false;
-          } else if (req.eventType !== "start") {
-            canStillReceivePending = false;
-            if (req.eventType === "aborted") {
-              // There's a small issue in the RxPlayer where init segments might
-              // be aborted synchronously before their success is anounced. This
-              // should have no effect on playback however.
-              // TODO fix it in the RxPlayer
-              let shouldIgnore = false;
-              for (let j = i + 1; j < requestInfo.length; j++) {
-                const reqNext = requestInfo[j];
-                if (isRequestingSameSegment(reqNext, req)) {
-                  if (reqNext.eventType === "success") {
-                    shouldIgnore = true;
-                  }
-                  break;
-                }
-              }
-              if (shouldIgnore) {
-                continue;
-              }
+          updatePendingTimeElt();
+          const newElt = strHtml`<tr>
+          ${timestampElt}
+          <td>pending</td>
+          ${pendingRequestInfo.element}
+          <td>${req.periodId}</td>
+          <td>${req.representationId}</td>
+          <td>
+            ${req.segmentStart === -1 ? "init" : req.segmentStart.toFixed(2)}
+          </td>
+          <td>
+            ${
+              req.segmentDuration === -1
+                ? "-"
+                : req.segmentDuration.toFixed(2)
             }
-            for (let j = i - 1; j >= 0; j--) {
-              const reqBase = requestInfo[j];
-              if (
-                isRequestingSameSegment(reqBase, req) &&
-                reqBase.eventType === "start"
-              ) {
-                tableElt.appendChild(
-                  strHtml`<tr>
-                  ${timestampElt}
-                  <td>${req.eventType}</td>
-                  <td>
-                    ${(req.timestamp - reqBase.timestamp).toFixed(2)}
-                  </td>
-                  <td>${req.periodId}</td>
-                  <td>${req.representationId}</td>
-                  <td>
-                    ${req.segmentStart === -1 ? "init" : req.segmentStart}
-                  </td>
-                  <td>
-                    ${req.segmentDuration === -1 ? "-" : req.segmentDuration}
-                  </td>
-                </tr>`
-                );
-                if (tableElt.childNodes.length >= MAX_REQ_ELEMENTS - 1) {
-                  requestDataElt.appendChild(tableElt);
-                  moduleBodyElt.classList.remove("empty");
-                  return;
+          </td>
+        </tr>`;
+          tableElt.appendChild(newElt);
+          canStillReceivePending = false;
+        } else if (req.eventType !== "start") {
+          canStillReceivePending = false;
+          if (req.eventType === "aborted") {
+            // There's a small issue in the RxPlayer where init segments might
+            // be aborted synchronously before their success is anounced. This
+            // should have no effect on playback however.
+            // TODO fix it in the RxPlayer
+            let shouldIgnore = false;
+            for (let j = i + 1; j < requestInfo.length; j++) {
+              const reqNext = requestInfo[j];
+              if (isRequestingSameSegment(reqNext, req)) {
+                if (reqNext.eventType === "success") {
+                  shouldIgnore = true;
                 }
                 break;
               }
             }
+            if (shouldIgnore) {
+              continue;
+            }
+          }
+          for (let j = i - 1; j >= 0; j--) {
+            const reqBase = requestInfo[j];
+            if (
+              isRequestingSameSegment(reqBase, req) &&
+              reqBase.eventType === "start"
+            ) {
+              tableElt.appendChild(
+                strHtml`<tr>
+                ${timestampElt}
+                <td>${req.eventType}</td>
+                <td>
+                  ${(req.timestamp - reqBase.timestamp).toFixed(2)}
+                </td>
+                <td>${req.periodId}</td>
+                <td>${req.representationId}</td>
+                <td>
+                  ${req.segmentStart === -1 ? "init" : req.segmentStart}
+                </td>
+                <td>
+                  ${req.segmentDuration === -1 ? "-" : req.segmentDuration}
+                </td>
+              </tr>`
+              );
+              if (tableElt.childNodes.length >= MAX_REQ_ELEMENTS - 1) {
+                requestDataElt.appendChild(tableElt);
+                moduleBodyElt.classList.remove("empty");
+                return;
+              }
+              break;
+            }
           }
         }
-        if (tableElt.childNodes.length === 1) {
-          displayNoRequestInformation();
-          return;
-        }
-        requestDataElt.appendChild(tableElt);
-        moduleBodyElt.classList.remove("empty");
-      },
+      }
+      if (tableElt.childNodes.length === 1) {
+        displayNoRequestInformation();
+        return;
+      }
+      requestDataElt.appendChild(tableElt);
+      moduleBodyElt.classList.remove("empty");
+    }
+    const unsubscribeHistory = state.subscribe(
+      stateProp,
+      renderRequestHistory,
+      true
+    );
+
+    const unsubscribeTimeRepresentation = configState.subscribe(
+      STATE_PROPS.TIME_REPRESENTATION,
+      renderRequestHistory,
       true
     );
     return {
@@ -184,6 +202,7 @@ export default function generateRequestHistoryModule(
         unsubscribeLogHistory();
         unsubscribeSelected();
         unsubscribeHistory();
+        unsubscribeTimeRepresentation();
       },
     };
 
